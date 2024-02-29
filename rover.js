bcee9fd9d47ead7077c43df6d3adecd9d64ad93f
Rupali Pakhale <rupalipakhale@rupalis-mbp.mynetworksettings.com>
2024-02-29 15:36:34 -0500

    class

--- rover.js
+++ rover.js
@@ -1,5 +1,50 @@
-class Rover {
-   // Write code here!
-}
-
-module.exports = Rover;
+class Rover{
+      // Write code here!
+      constructor(position) {
+          this.position = position;
+          this.mode = 'NORMAL';
+          this.generatorWatts = 110;
+      }
+  
+      receiveMessage(message) {
+          let results = [];
+  
+          for (let i = 0; i < message.commands.length; i++) {
+              let command = message.commands[i];
+              let result = {};
+  
+              if (command.commandType === 'MODE_CHANGE') {
+                  this.mode = command.value;
+                  result.completed = true;
+                  results.push(result);
+              } else if (command.commandType === 'MOVE') {
+                  if (this.mode === 'NORMAL') {
+                      this.position = command.value;
+                      result.completed = true;
+                      results.push(result);
+                  } else if (this.mode === 'LOW_POWER') {
+                      result.completed = false;
+                      results.push(result);
+                  }
+              } else if (command.commandType === 'STATUS_CHECK') {
+                  result.completed = true;
+                  result.roverStatus = {
+                      mode: this.mode,
+                      generatorWatts: this.generatorWatts,
+                      position: this.position,
+                  };
+                  results.push(result);
+              } else {
+                  
+              }
+          }
+  
+          return {
+              message: message.name,
+              results: results,
+          };
+      }
+  }
+  
+  module.exports = Rover;
+  
